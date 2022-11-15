71437ceb83693b670472bc7204e2bbe717bca3fe
Tatiana <taty07@gmail.com>
2022-11-15 15:28:55 +0100

    iteration1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsJSON from "./contacts.json";
+// import { useState } from "react";
 
 function App() {
+  const fiveContacts = contactsJSON.slice(0, 5);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <h3>Picture</h3>
+            </th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+            <th>
+              <h3>Name</h3>
+            </th>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+            <th>
+              <h3>Popularity</h3>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {fiveContacts.map((contactsJSON) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contactsJSON.pictureUrl} />
+                </td>
+                <td>
+                  <p>{contactsJSON.name}</p>
+                </td>
+                <td>
+                  <p>{contactsJSON.popularity}</p>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
